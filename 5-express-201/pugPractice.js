351e59af6a19ae63e8b2c1f33657443e41767fd1
Matthijs Boet <mboet@client13.ict.fqdn>
2021-08-16 15:29:27 +0200

    ¨

--- 5-express-201/pugPractice.js
+++ 5-express-201/pugPractice.js
@@ -12,13 +12,27 @@ app.use(express.urlencoded({
     extended: true
 }))
 
-app.set("view engine", "ejs")
+app.set("view engine", "pug")
 app.set("views", path.join(__dirname, "views"))
 
 app.get('/', (req, res, next) => {
     // The data in 2nd arg (render) is appended to res.locals
     res.render("index", {
-        
+        countries: [
+            {
+                name: `Russia`,
+                capital: `Moscow`,
+                western: false
+            },
+            {
+                name: `England`,
+                capital: `London`,
+                western: true
+            }
+        ],
+        msg: `Failure!!!`,
+        msg2: `Success!!!`,
+        html: `<p><img src="https://tracks-trails.com/wp-content/uploads/2019/09/Yosemite-National-Park.jpg" alt="" /></p>`
     })
 })
 
