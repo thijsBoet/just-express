78e98810af776a4feac76da2c9afe6cc0266a7b8
Matthijs Boet <m.boet@chello.nl>
2021-08-16 11:57:13 +0200

    practicing ejs

--- 5-express-201/ejsPractice.js
+++ 5-express-201/ejsPractice.js
@@ -16,7 +16,11 @@ app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 
 app.get('/', (req, res, next) => {
-    res.render("index")
+    // The data in 2nd arg (render) is appended to res.locals
+    res.render("index", {
+        msg: "Success!!!",
+        msg2: "Failure!!!"
+    })
 })
 
 app.listen(3000, () => console.log(`Listening on port 3000`))
