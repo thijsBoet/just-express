d23ac34c0410e410ee36f70a069f5ed99ef1a21d
Matthijs Boet <m.boet@chello.nl>
2021-08-22 21:23:57 +0200

    starting auth chapter 9

--- 8-movie-api/routes/movie.js
+++ 8-movie-api/routes/movie.js
@@ -3,6 +3,30 @@ var router = express.Router();
 
 const movieDetails = require('../data/movieDetails');
 
+function requireJSON(req, res, next) {
+    if (!req.is("application/json")) {
+        res.json({
+            msg: "Content type must be application/json"
+        })
+    } else {
+        next();
+    }
+}
+
+router.param(("movieId"), (req, res, next) => {
+    // update the DB with analytics data
+    console.log("movieId wildcard route hit")
+    next();
+})
+
+router.get("/top_rated", (req, res, next) => {
+    let page = req.query.page;
+    if (!page) page = 0;
+    const results = movieDetails.sort((a, b) => b.vote_average - a.vote_average);
+    const indexToStart = (page - 1) * 20;
+    res.json(results.slice(indexToStart, indexToStart + 19));
+})
+
 /* GET movie page. */
 router.get("/:movieId", (req, res, next) => {
     const movieId = req.params.movieId;
@@ -16,5 +40,24 @@ router.get("/:movieId", (req, res, next) => {
         res.json(results)
     }
 })
+
+router.post("/:movieId/rating", requireJSON, (req, res, next) => {
+    const movieId = req.params.movieId;
+    const userRating = req.body.value;
+    if ((userRating < .5) || (userRating > 10)) {
+        res.json({ msg: "Rating must be between 0.5 and 10." })
+    } else {
+        res.json({
+            msg: "Thank you for submitting your rating",
+            status_code: 200
+        })
+    }
+})
+
+router.delete("/:movieId/rating", requireJSON, (req, res, next) => {
+    res.json({msg: "Rating deleted!"})
+})
+
+
 
 module.exports = router;
--- 8-movie-api/routes/search.js
+++ 8-movie-api/routes/search.js
@@ -1,11 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET search page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {
-        title: 'Express'
-    });
+const movies = require('../data/movies');
+const people = require('../data/people');
+
+router.use(queryRequired = (req, res, next) => {
+    const searchTerm = req.query.query;
+    if (!searchTerm) {
+        res.json({
+            msg: "Query is required"
+        })
+    } else {
+        next();
+    }
+})
+
+router.get('/movie', (req, res, next) => {
+    const searchTerm = req.query.query;
+    const results = movies.filter(movie => {
+        let found = movie.overview.includes(searchTerm) || movie.title.includes(searchTerm);
+        return found;
+    })
+    res.json({results})
 });
+
+router.get('/person', (req, res, next) => {
+    const searchTerm = req.query.query;
+    const results = people.filter(person => {
+        let found = person.name.includes(searchTerm);
+        return found;
+    })
+    res.json({
+        results
+    })
+});
+
+
 
 module.exports = router;
